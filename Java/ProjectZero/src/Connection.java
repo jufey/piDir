f6ac5ec9dbc760e95f4812b23941a5de3a06fc64
Justin Marks <justin_m@live.de>
2015-09-02 10:18:09 +0200

    Reworked

--- Java/ProjectZero/src/Connection.java
+++ Java/ProjectZero/src/Connection.java
@@ -2,49 +2,69 @@ import java.io.*;
 import java.net.*;
 
 public class Connection extends Thread {
-    protected EchoServer server;			// Link zum Server
-    protected Socket socket;			// Socket der Verbindung
-    protected BufferedReader sin;			// Eingabestream für Tastatureingaben
-    protected PrintWriter sout;			// Ausgabestream auf Socket
+    protected EchoServer server;
+    protected Socket socket;
+    protected BufferedReader sin;
+    protected PrintWriter sout;
 
     public Connection(Socket socket, EchoServer server) {
         this.socket = socket;
         this.server = server;
-        System.out.println(socket + ": Neue Verbindung");
-        try {					// Streams erzeugen
+        System.out.println(socket + ": Connected");
+        try {
             sin = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             sout = new PrintWriter(socket.getOutputStream());
+        } catch (IOException e) {
+            try {
+                socket.close();
+            } catch (IOException e2) {
+            }
+            System.err.println("Error occurred while creating a Connection: " + e);
+            return;
         }
-        catch (IOException e) {
-            try { socket.close(); } 	// Socket noch geöffnet? Dann schließen
-            catch (IOException e2) {}
-            System.err.println("Ausnahme beim Bereitstellen der Socketstreams: " + e);
-            return;				// Bei Fehler Methode beenden
-        }
-        this.start();				// Thread starten
+        this.start();
     }
 
-    public void run () {
+    public void run() {
         String line;
         try {
             while (true) {
-                line = sin.readLine();		// Auf eine Textzeile vom Client warten
-                if (line == null) break;	// Wenn String leer, dann wurde Verbindung beendet
-                server.receive(line, this);	// empfangene Daten an Server übergeben
+                line = sin.readLine();
+                if (line == null) break;
+                server.receive(line, this);
+                if (line.equalsIgnoreCase("Close connection")) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }else if(line.equalsIgnoreCase("Server shutdown")){
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
             }
-        }
-        catch(IOException e) {}
-        finally {
+        } catch (IOException e) {
+            System.out.println("Error while receiving.");
+        } finally {
             System.out.print(socket + ": Verbindung schließen...");
-            try { socket.close(); System.out.println("OK!");}
-            catch(IOException e2) { System.out.println("ERR!"); }
+            try {
+                socket.close();
+                System.out.println("OK!");
+            } catch (IOException e2) {
+                System.out.println("ERR!");
+            }
         }
     }
 
     public void send(String line) {
-        if (sout != null) {				// Wenn Ausgabestream noch aktiv
-            sout.println(line);			// Daten an Client ausgeben
-            sout.flush();				// und sofort senden
+        if (sout != null) {                // Wenn Ausgabestream noch aktiv
+            sout.println(line);            // Daten an Client ausgeben
+            sout.flush();                // und sofort senden
         }
     }
 }
--- Java/ProjectZero/src/EchoServer.java
+++ Java/ProjectZero/src/EchoServer.java
@@ -3,41 +3,40 @@ import java.net.*;
 import java.util.*;
 
 public class EchoServer extends Thread {
-    public static final int DEFAULT_PORT = 33333;	// Default-Portnummer
-    int port;					// aktuelle Portnummer
-    ServerSocket server_socket;			// Der Socket des Servers
-    Vector connections;				// Vektor aller aktiven Verbindungen
+    public static final int DEFAULT_PORT = 61509;
+    ServerSocket server_socket;
+    Vector<Connection> connections;
     int num_connections;
+    private boolean isRunning;
 
     public static void fail(Exception e, String msg) {
         System.err.println(msg + ": " + e);
         System.exit(1);
     }
 
-    public EchoServer(int port) {
-        this.port = port;			// Port initialisieren
-        connections = new Vector();
+    public EchoServer() {
+        connections = new Vector<Connection>();
         try {
-            server_socket = new ServerSocket(port);
+            server_socket = new ServerSocket(DEFAULT_PORT);
             System.out.println(server_socket +
-                    "\nServer lauscht auf Port " + port);
-        }
-        catch(IOException e) {
-            fail(e, "Eine Ausnahme trat beim Anlegen des Server Socket auf");
+                    "\n" +
+                    "Server is listening on " + DEFAULT_PORT);
+            isRunning = true;
+        } catch (IOException e) {
+            fail(e, "Error while starting the Server");
         }
         this.start();
     }
 
     public void run() {
         try {
-            while(true) {
+            while (isRunning) {
                 Socket client_socket = server_socket.accept();
                 Connection c = new Connection(client_socket, this);
                 addConnection(c);
             }
-        }
-        catch(IOException e) {
-            fail(e, "Beim Warten auf Verbindungen ist eine Ausnahme aufgetreten");
+        } catch (IOException e) {
+            fail(e, "While waiting for a connection an error occurred");
         }
     }
 
@@ -49,7 +48,7 @@ public class EchoServer extends Thread {
     public synchronized void receive(String line, Connection c) {
         c.send(line);
         System.out.println(line);
-        if(line.equalsIgnoreCase("exit")){
+        if (line.equalsIgnoreCase("Server shutdown")) {
             try {
                 server_socket.close();
                 System.out.println("Server shutdown");
@@ -61,6 +60,6 @@ public class EchoServer extends Thread {
     }
 
     public static void main(String[] args) {
-        new EchoServer(DEFAULT_PORT);
+        new EchoServer();
     }
 }
