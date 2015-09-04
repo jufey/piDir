52d9fc3c262818f618cf0ddb2f8d39ccd69f9d5f
Justin Marks <justin_m@live.de>
2015-09-04 23:44:46 +0200

    PWM test

--- Java/ProjectZero/src/tests1.java
+++ Java/ProjectZero/src/tests1.java
@@ -2,32 +2,42 @@
  * Created by jufey on 02/09/15.
  */
 
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.*;
 
 
 public class tests1 {
 
     public static void main(String[] args) throws InterruptedException {
         final GpioController gpio = GpioFactory.getInstance();
-        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+        final GpioPinPwmOutput pin = gpio.provisionPwmOutputPin(RaspiPin.GPIO_01, "MyLED");
+//        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
         pin.setShutdownOptions(true, PinState.LOW);
-
-        System.out.println("--> GPIO state should be: ON");
+//
+        pin.setPwm(0);
+        Thread.sleep(5000);
+        pin.setPwm(20);
+        Thread.sleep(5000);
+        pin.setPwm(40);
+        Thread.sleep(5000);
+        pin.setPwm(80);
+        Thread.sleep(5000);
+        pin.setPwm(100);
+        Thread.sleep(5000);
+        pin.setPwm(0);
         Thread.sleep(5000);
 
-        pin.low();
-        System.out.println("--> GPIO state should be: OFF");
-
-        Thread.sleep(5000);
-        pin.high();
-        System.out.println("--> GPIO state should be: ON");
-
-        Thread.sleep(5000);
-
+//        System.out.println("--> GPIO state should be: ON");
+//        Thread.sleep(5000);
+//
+//        pin.low();
+//        System.out.println("--> GPIO state should be: OFF");
+//
+//        Thread.sleep(5000);
+//        pin.high();
+//        System.out.println("--> GPIO state should be: ON");
+//
+//        Thread.sleep(5000);
+//
         gpio.shutdown();
 
     }
